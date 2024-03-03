4e445ca0248da6bb7d9a2ca81dc3874986853dde
Draycien <156354951+Draycien@users.noreply.github.com>
2024-03-03 02:04:55 +0200

    Delete database.js

--- database.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql2');
-const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'test_user',
-  password: 'jose',
-  database: 'test_db'
-});
-module.exports = connection;
